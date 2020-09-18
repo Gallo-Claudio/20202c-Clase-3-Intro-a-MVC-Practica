b6e8ef0e1442921e0a16968db5ffe608b5474f31
Gallo <g>
2020-09-17 22:18:00 -0300

    Listar todos - Hardcodeado

---
+++ Intro MVC/Controllers/AlimentosController.cs
@@ -0,0 +1,35 @@
+﻿using Entidades;
+using Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intro_MVC.Controllers
+{
+    public class AlimentosController : Controller
+    {
+        // GET: Alimentos
+        public ActionResult Alta()
+        {
+            return View();
+        }
+
+        public ActionResult Todos()
+        {
+            List<Alimento> Listado = AlimentosServicios.Hardcodeo();
+            return View(Listado);
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Borrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Servicios/AlimentosServicios.cs
@@ -0,0 +1,43 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class AlimentosServicios
+    {
+        private static List<Alimento> ListaAlimento = new List<Alimento>();
+
+        public void Alta(string Nombre, int Peso)
+        {
+            Alimento alimento = new Alimento();
+            alimento.Nombre = Nombre;
+            alimento.Peso = Peso;
+
+            ListaAlimento.Add(alimento);
+        }
+
+        public static List<Alimento> Hardcodeo()
+        {
+            Alimento alimento = new Alimento();
+            alimento.Nombre = "Papa";
+            alimento.Peso = 100;
+            ListaAlimento.Add(alimento);
+
+            Alimento alimento2 = new Alimento();
+            alimento2.Nombre = "Tomate";
+            alimento2.Peso = 500;
+            ListaAlimento.Add(alimento2);
+
+            Alimento alimento3 = new Alimento();
+            alimento3.Nombre = "Porotos";
+            alimento3.Peso = 300;
+            ListaAlimento.Add(alimento3);
+
+            return ListaAlimento;
+        }
+    }
+}
